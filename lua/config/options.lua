b9912289a42616cffa3dd1914dd1365f5131196c
Gianmarco Andreana <42800762+GianmarcoAndreana@users.noreply.github.com>
2024-09-18 23:24:46 +0200

    moved to Windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,19 +3,40 @@
 -- Add any additional options here
 
 -- WSL Clipboard support
-if is_wsl then
-  -- This is NeoVim's recommended way to solve clipboard sharing if you use WSL
-  -- See: https://github.com/neovim/neovim/wiki/FAQ#how-to-use-the-windows-clipboard-from-wsl
-  vim.g.clipboard = {
-    name = "WslClipboard",
-    copy = {
-      ["+"] = "clip.exe",
-      ["*"] = "clip.exe",
-    },
-    paste = {
-      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    },
-    cache_enabled = 0,
-  }
+--if is_wsl then
+--  -- This is NeoVim's recommended way to solve clipboard sharing if you use WSL
+--  -- See: https://github.com/neovim/neovim/wiki/FAQ#how-to-use-the-windows-clipboard-from-wsl
+--  vim.g.clipboard = {
+--    name = "WslClipboard",
+--    copy = {
+--      ["+"] = "clip.exe",
+--      ["*"] = "clip.exe",
+--    },
+--    paste = {
+--      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--    },
+--    cache_enabled = 0,
+--  }
+
+-- Use Powershell in LazyTerm
+-- Check if 'pwsh' is executable and set the shell accordingly
+if vim.fn.executable("pwsh") == 1 then
+  vim.o.shell = "pwsh"
+else
+  vim.o.shell = "powershell"
 end
+
+-- Setting shell command flags
+vim.o.shellcmdflag =
+  "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
+
+-- Setting shell redirection
+vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
+
+-- Setting shell pipe
+vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
+
+-- Setting shell quote options
+vim.o.shellquote = ""
+vim.o.shellxquote = ""
